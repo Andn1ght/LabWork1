019dfc747688ef2c98c5ae5a0984f919fd89dbb2
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 04:18:55 +0600

    Adding new case with implementation of AverageFinder in main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.AverageFinder;
 import algorithms.MinFinder;
 
 import java.util.Scanner;
@@ -8,6 +9,7 @@ public class Main {
 
         System.out.println("Which problem do you want to solve?");
         System.out.println("1. Find the minimum element");
+        System.out.println("2. Find the average of the elements");
         System.out.print("Enter the problem number: ");
 
         int pNum = input.nextInt();
@@ -29,6 +31,22 @@ public class Main {
                 int min = minFinder.findMin(arr, n);
 
                 System.out.println("Minimum element is: " + min);
+                break;
+            case 2:
+                System.out.print("Enter the number of elements: ");
+                int n2 = input.nextInt();
+
+                int[] arr2 = new int[n2];
+
+                for (int i = 0; i < n2; i++) {
+                    System.out.print("Enter element " + (i + 1) + ": ");
+                    arr2[i] = input.nextInt();
+                }
+
+                AverageFinder averageFinder = new AverageFinder();
+                double avg = averageFinder.findAverage(arr2, n2);
+
+                System.out.println("Average number is: " + avg);
                 break;
             default:
                 System.out.println("Invalid problem number");
