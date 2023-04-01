c6f6953d09bcefc06e827f35013a55d2f1fb7d28
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 03:58:43 +0600

    main class that asks from user which problem do he wants to solve

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import algorithms.MinFinder;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Which problem do you want to solve?");
+        System.out.println("1. Find the minimum element");
+        System.out.print("Enter the problem number: ");
+
+        int pNum = input.nextInt();
+
+        switch (pNum) {
+
+            case 1:
+                System.out.print("Enter the number of elements: ");
+                int n = input.nextInt();
+
+                int[] arr = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    System.out.print("Enter element " + (i + 1) + ": ");
+                    arr[i] = input.nextInt();
+                }
+
+                MinFinder minFinder = new MinFinder();
+                int min = minFinder.findMin(arr, n);
+
+                System.out.println("Minimum element is: " + min);
+                break;
+            default:
+                System.out.println("Invalid problem number");
+        }
     }
 }
