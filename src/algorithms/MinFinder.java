742133bdbb5d50f411620d5f277a5eb9dd34289e
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 03:42:55 +0600

    recursive method to find minimum element of array

--- src/algorithms/MinFinder.java
+++ src/algorithms/MinFinder.java
@@ -1,5 +1,11 @@
 package algorithms;
 
 public class MinFinder {
+    public int findMin(int[] arr, int n) {
 
+        if (n == 1) return arr[0];
+
+        else return Math.min(arr[n-1], findMin(arr, n-1));
+
+    }
 }
