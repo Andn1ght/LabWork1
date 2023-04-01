9e70b8890d365eafebe314bdff4785351f3ef61a
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 04:07:20 +0600

    implementation of this class

--- src/algorithms/AverageFinder.java
+++ src/algorithms/AverageFinder.java
@@ -1,5 +1,12 @@
 package algorithms;
 
 public class AverageFinder {
+    public double findAverage(int[] arr, int n){
 
+        if (n == 0) return 0;
+        else{
+            double prev = findAverage(arr, n-1);
+            return (prev * (n-1) + arr[n-1]) / n;
+        }
+    }
 }
