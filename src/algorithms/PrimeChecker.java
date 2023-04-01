33bd6745674506c9166e6c65a915a0a65ec7257e
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 04:25:29 +0600

    given a number n, i is current divisor to check, returns true if n is prime, else returns false.

--- src/algorithms/PrimeChecker.java
+++ src/algorithms/PrimeChecker.java
@@ -1,4 +1,14 @@
 package algorithms;
 
 public class PrimeChecker {
+    public boolean isPrime(int n, int i){
+
+        if (n == 1 || n == 0) return false;
+        if (n == i) return true;
+
+        if (n % i == 0) return false;
+
+        return isPrime(n, i+1);
+
+    }
 }
