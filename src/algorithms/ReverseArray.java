7442360827fbbe5b74607a5c5ac71521ef6e12b1
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 21:22:22 +0600

    class ReverseArray has new method to reverse elements of the "arr" array

--- src/algorithms/ReverseArray.java
+++ src/algorithms/ReverseArray.java
@@ -1,4 +1,14 @@
 package algorithms;
 
 public class ReverseArray {
+    public void reverse(int[] arr, int a, int n){
+
+        if (a >= n) return;
+
+        int temp = arr[a];
+        arr[a] = arr[n];
+        arr[n] = temp;
+        reverse(arr, a+1, n-1);
+
+    }
 }
