89502ea11ed6e176671740d3c6e246914a97edad
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 21:39:30 +0600

    implementation of this class by creating new method isDigit

--- src/algorithms/DigitChecker.java
+++ src/algorithms/DigitChecker.java
@@ -1,4 +1,15 @@
 package algorithms;
 
 public class DigitChecker {
+    public boolean isDigit(String str){
+
+        if (str.isEmpty()) return true;
+
+        char ch = str.charAt(0);
+
+        if (ch < '0' || ch > '9') return false;
+
+        return isDigit(str.substring(1));
+
+    }
 }
