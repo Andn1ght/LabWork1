6746ccd2ec086900da7d72b3c16963168c79dd50
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 22:55:35 +0600

    simple implementation of GCD calculator

--- src/algorithms/GCDFinder.java
+++ src/algorithms/GCDFinder.java
@@ -1,4 +1,12 @@
 package algorithms;
 
 public class GCDFinder {
+    public int GCD(int a, int b) {
+
+        if (b == 0)
+            return a;
+        else
+            return GCD(b, a % b);
+
+    }
 }
