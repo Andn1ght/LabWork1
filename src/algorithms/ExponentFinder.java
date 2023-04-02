75e2441b7c1e6395adbf2a9dc6906902094eed14
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 21:09:07 +0600

    here's the function for finding the result of "a" to the power of "n"

--- src/algorithms/ExponentFinder.java
+++ src/algorithms/ExponentFinder.java
@@ -1,4 +1,12 @@
 package algorithms;
 
 public class ExponentFinder {
+    public int exp(int a, int n){
+
+        if (n == 0)
+            return 1;
+        else
+            return a * exp(a, n-1);
+
+    }
 }
