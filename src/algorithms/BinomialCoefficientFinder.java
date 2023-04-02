0e2321adeefdcbc60ac7e808e2eecbb6bbc380e2
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-04-02 22:45:16 +0600

    implementation of a method for finding binomial coefficient

--- src/algorithms/BinomialCoefficientFinder.java
+++ src/algorithms/BinomialCoefficientFinder.java
@@ -1,4 +1,11 @@
 package algorithms;
 
 public class BinomialCoefficientFinder {
+    public int binom(int n, int k){
+
+        if (k == 0 || k == n) return 1;
+
+        return binom(n-1, k-1) + binom(n-1, k);
+
+    }
 }
